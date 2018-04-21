8982ab0eeb35f1d7d73bf6db0157714bf6c10b22
Fernando Concatto <fernandoconcatto@gmail.com>
2018-04-20 22:22:10 -0300

    Apresentado

--- IDE/src/api/Semantico.java
+++
@@ -1,7 +0,0 @@
-package api;public class Semantico implements Constants
-{
-    public void executeAction(int action, Token token)	throws SemanticError
-    {
-        System.out.println("Ação #"+action+", Token: "+token);
-    }	
-}
